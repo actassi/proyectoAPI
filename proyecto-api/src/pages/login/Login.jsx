5a2f864483c035aa8a327d9e37ddb526ee0231c7
Nahuel <cabralnahuel.dev@gmail.com>
2024-05-07 15:01:24 -0300

    tipografia y estilo al login

--- proyecto-api/src/pages/login/Login.jsx
+++ proyecto-api/src/pages/login/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import imagen from "../../assets/logo.png";
 import appFirebase from "../../utils/credenciales";
 import "./login.css";
 import {
@@ -9,11 +8,14 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import PopUp from "./PopUp";
+import NavLogin from "./nav";
 
 const auth = getAuth(appFirebase);
 
 const Login = () => {
   const [registrando, setRegistrando] = useState(false);
+  const [mostrarPopUp, setMostrarPopUp] = useState(false); 
 
   const funcionDeAutenticacion = async (e) => {
     e.preventDefault();
@@ -29,6 +31,9 @@ const Login = () => {
       }
     }
   };
+  const togglePopUp = () => {
+    setMostrarPopUp(!mostrarPopUp); // Alternar la visibilidad del PopUp
+  };
 
   async function registrarseConGoogle() {
     const provider = new GoogleAuthProvider();
@@ -42,15 +47,14 @@ const Login = () => {
 
   return (
     
-    <div className="h-100">
-      <div className="row align-items-center justify-content-center h-100">
-        <div className="col-md-5 col-sm-3">
-          <img src={imagen} alt="" className="img-fluid" />
-        </div>
-        <div className="col-md-6">
-          <div className="registro row gap-2 text-center m-2 " >
-            <h1 className="titulo display-6">Lo que está pasando ahora</h1>
-            <h4 className="subtitulo">Unete Hoy</h4>
+    <div className="h-100 loginContenedorPrincipal">
+      <div className="loginContenedor2">
+        <div className="x">
+        <svg viewBox="0 0 24 24" className="x"><g><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg>        </div>
+        <div className="">
+          <div className="contenedorTitulos " >
+            <h1 className="titulo">Lo que está pasando ahora</h1>
+            <h4 className="subtitulo">Únete Hoy</h4>
             <button type="button" className="botonAutenticacion mr-3" onClick={registrarseConGoogle}>
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="16" height="16" ><g><path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path><path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path><path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path><path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path><path fill="none" d="M0 0h48v48H0z"></path></g></svg>
 Registrarse con Google
@@ -62,29 +66,27 @@ Registrarse con Google
               Registrarse con Apple
             </button>
             <p className="or">o</p>
-            <button className="botonAutenticacion">Crear cuenta</button>
+            <button className="botonAutenticacion crearCuenta">Crear cuenta</button>
             <div  className="terminosycondiciones">Al registrarte, aceptas los <a href="https://twitter.com/tos">Términos de servicio</a> y la <a href="https://twitter.com/privacy" >Política de privacidad</a>, incluida la política de <a href="https://help.twitter.com/rules-and-policies/twitter-cookies" >Uso de Cookies</a>.</div>
              
           </div>
-          <div className="card card-body">
-            <form onSubmit={funcionDeAutenticacion}>
-              <input type="text" placeholder="Ingresar email" id="email" />
-              <input
-                type="password"
-                placeholder="Ingresar Contraseña"
-                id="password"
-              />
-              <button>{registrando ? "Registrarse" : "Iniciar Sesion"}</button>
-            </form>
-            <h4>
-              ¿ya tienes cuenta?
-              <button onClick={() => setRegistrando(!registrando)}>
-                {registrando ? "Iniciar Sesion" : "Registrarse"}
+          <div className="yaTienesCuenta">
+        
+            <h4 className="h4Login">
+              ¿Ya tienes cuenta?
+              </h4>
+        
+              <button className="botonAutenticacion yaTienesCuenta__boton" onClick={togglePopUp}>
+                
+                {!registrando ? "Iniciar Sesion" : "Registrarse"}
               </button>
-            </h4>
+              
           </div>
         </div>
       </div>
+      {mostrarPopUp && <PopUp />} {/* Renderiza el PopUp solo si mostrarPopUp es true */}
+
+      <div className="Nav"><NavLogin/></div>
     </div>
   );
 };
