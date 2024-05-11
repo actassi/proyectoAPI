ae715838933e13145b1932d0ad93b4cf7502cd0c
Andrea <actassi@gmail.com>
2024-05-11 20:52:51 -0300

    actualizando profile

--- proyecto-api/src/pages/profile/EditProfile.jsx
+++ proyecto-api/src/pages/profile/EditProfile.jsx
@@ -138,16 +138,20 @@ const EditProfile = () => {
   });
 
   const [nombre, setNombre] = useState("");
-  const [apellido, setApellido] = useState("");
-  const [nombreUsuario, setNombreUsuario] = useState("");
+  const [biografia, setBiografia] = useState("");
+  const [ubicacion, setUbicacion] = useState("");
+  const [web, setWeb] = useState("");
 
   const editarUsuarios = async (e) => {
     e.preventDefault();
     const usuario = {
       nombre: nombre,
-      apellidos: apellido,
-      nombreUsuario: nombreUsuario,
-    };
+      biografia: biografia,
+      ubicacion: ubicacion,
+      web: web,
+      correo: user.email
+     };
+     
     const usuarios = collection(db, "usuarios");
     console.log(usuario);
     await addDoc(usuarios, usuario);
@@ -177,29 +181,47 @@ const EditProfile = () => {
                 autoFocus
               />
               <TextField
-                value={apellido}
-                onChange={(e) => setApellido(e.target.value)}
+                value={biografia}
+                onChange={(e) => setBiografia(e.target.value)}
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                id="apellido"
-                label="Apellido"
-                name="apellido"
+                id="biografia"
+                label="Biografía"
+                name="biografia"
                 type="text"
                 autoComplete="text"
               />
               <TextField
-                value={nombreUsuario}
-                onChange={(e) => setNombreUsuario(e.target.value)}
+                value={ubicacion}
+                onChange={(e) => setUbicacion(e.target.value)}
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                id="usuario"
-                label="Nombre de Usuario"
-                name="usuario"
+                id="ubicacion"
+                label="Ubicación"
+                name="ubicacion"
                 type="text"
                 autoComplete="text"
               />
+
+              <TextField
+                value={web}
+                onChange={(e) => setWeb(e.target.value)}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="web"
+                label="Sitio web"
+                name="web"
+                type="text"
+                autoComplete="text"
+              />
+
+              <div>
+                <p>Fecha de Nacimiento</p>           
+
+              </div>
 
               <Button
                 type="submit"
