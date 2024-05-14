7b9b1895d418debd5829c7782d0cb17d08ae77d0
Andrea <actassi@gmail.com>
2024-05-14 13:03:38 -0300

    mandar y recibir mensajes ok, en orden y actualización

--- proyecto-api/src/components/cards/CardMandarMensajes.jsx
+++ proyecto-api/src/components/cards/CardMandarMensajes.jsx
@@ -1,6 +1,209 @@
+// import { Avatar, Button, Card, CardContent, CardHeader, Grid, TextField } from '@mui/material';
+// import { useState } from "react";
+// import { collection, addDoc } from "firebase/firestore";
+// import { db } from "../../firebase/Conexion.js";
+// import { getAuth } from "firebase/auth";
+
+// const MandarMensajes = ({ onMessageSent }) => {
+//   const auth = getAuth();
+//   const user = auth.currentUser;
+//   const avatar = user.photoURL;
+//   const name = user.displayName;
+  
+//   // Agregar timestamp al mensaje
+//   const [texto, setTexto] = useState("");
+//   const [correo, setCorreo] = useState(user && user.email ? user.email : "");
+
+//   const crearMensaje = async (e) => {
+//     e.preventDefault();
+//     const mensaje = {
+//       texto: texto,
+//       correo: correo,
+//       photoURL: avatar // Asegúrate de agregar la URL de la foto al mensaje
+//     };
+
+//     try {
+//       const mensajes = collection(db, "mensajes");
+//       const docRef = await addDoc(mensajes, mensaje);
+//       mensaje.id = docRef.id; // Agregar el ID del documento generado a la propiedad mensaje
+//       onMessageSent(mensaje); // Llamar a la función onMessageSent con el nuevo mensaje
+//       setTexto(""); // Limpiar el campo de texto después de enviar el mensaje
+//     } catch (error) {
+//       console.error("Error adding document: ", error);
+//     }
+//   };
+
+//   return (
+//     <Card sx={{ width: '100%' }}>
+//       <CardHeader
+//         avatar={
+//           <Avatar 
+//             alt={name}
+//             src={avatar}
+//             sx={{ width: 48, height: 48 }} 
+//           />
+//         }
+//       />
+//       <CardContent>
+//         <Grid container spacing={2} alignItems="center">
+//           <Grid item xs={12} sm={12}>
+//             <TextField
+//               fullWidth
+//               variant="outlined"
+//               placeholder="Escribe tu mensaje..."
+//               value={texto}
+//               onChange={(e) => setTexto(e.target.value)}
+//               sx={{
+//                 '& .MuiOutlinedInput-root': {
+//                   borderRadius: '999px',
+//                   backgroundColor: 'transparent',
+//                   '& fieldset': {
+//                     borderColor: 'transparent',
+//                   },
+//                   '&:hover fieldset': {
+//                     borderColor: 'transparent',
+//                   },
+//                   '&.Mui-focused fieldset': {
+//                     borderColor: 'transparent',
+//                   },
+//                   '& input': {
+//                     color: 'black', // Ajustar el color del texto según sea necesario
+//                   },
+//                   '&::placeholder': {
+// //                     color: 'gray', // Ajustar el color del placeholder según sea necesario
+// //                   },
+// //                 },
+// //               }}
+// //             />
+// //           </Grid>
+// //           <Grid item xs={12} sm={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+// //             <Button
+// //               onClick={crearMensaje} 
+// //               variant="contained"
+// //               sx={{
+// //                 borderRadius: '999px', 
+// //                 bgcolor: '#7abaff', 
+// //                 '&:hover': {
+// //                   bgcolor: '#59a3f7', 
+// //                 },
+// //               }}
+// //             >
+// //               Enviar
+// //             </Button>
+// //           </Grid>
+// //         </Grid>
+// //       </CardContent>
+// //     </Card>
+// //   );
+// // };
+
+// // export default MandarMensajes;
+
+
+// import { Avatar, Button, Card, CardContent, CardHeader, Grid, TextField } from '@mui/material';
+// import { useState } from "react";
+// import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+// import { db } from "../../firebase/Conexion.js";
+// import { getAuth } from "firebase/auth";
+
+// const MandarMensajes = ({ onMessageSent }) => {
+//   const auth = getAuth();
+//   const user = auth.currentUser;
+//   const avatar = user.photoURL;
+//   const name = user.displayName;
+
+//   const [texto, setTexto] = useState("");
+//   const [correo, setCorreo] = useState(user && user.email ? user.email : "");
+
+//   const crearMensaje = async (e) => {
+//     e.preventDefault();
+//     const mensaje = {
+//       texto: texto,
+//       correo: correo,
+//       timestamp: serverTimestamp() // Agregar el timestamp al mensaje
+//     };
+    
+//     try {
+//       const mensajes = collection(db, "mensajes");
+//       const docRef = await addDoc(mensajes, mensaje);
+//       mensaje.id = docRef.id; // Agregar el ID del documento generado al mensaje
+//       onMessageSent(mensaje); // Llamar a la función onMessageSent con el nuevo mensaje
+//       setTexto(""); // Limpiar el campo de texto después de enviar el mensaje
+//     } catch (error) {
+//       console.error("Error adding document: ", error);
+//     }
+//   };
+
+//   return (
+//     <Card sx={{ width: '100%' }}>
+//       <CardHeader
+//         avatar={
+//           <Avatar 
+//             alt={name}
+//             src={avatar}
+//             sx={{ width: 48, height: 48 }} 
+//           />
+//         }
+//       />
+//       <CardContent>
+//         <Grid container spacing={2} alignItems="center">
+//           <Grid item xs={12} sm={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+//             <TextField
+//               fullWidth
+//               variant="outlined"
+//               placeholder="Escribe tu mensaje..."
+//               value={texto}
+//               onChange={(e) => setTexto(e.target.value)}
+//               sx={{
+//                 '& .MuiOutlinedInput-root': {
+//                   borderRadius: '999px',
+//                   backgroundColor: 'transparent',
+//                   '& fieldset': {
+//                     borderColor: 'transparent',
+//                   },
+//                   '&:hover fieldset': {
+//                     borderColor: 'transparent',
+//                   },
+//                   '&.Mui-focused fieldset': {
+//                     borderColor: 'transparent',
+//                   },
+//                   '& input': {
+//                     color: 'black', // Ajustar el color del texto según sea necesario
+//                   },
+//                   '&::placeholder': {
+//                     color: 'gray', // Ajustar el color del placeholder según sea necesario
+//                   },
+//                 },
+//               }}
+//             />
+//           </Grid>
+//           <Grid item xs={12} sm={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+//             <Button
+//               onClick={crearMensaje} 
+//               variant="contained"
+//               sx={{
+//                 borderRadius: '999px', 
+//                 bgcolor: '#7abaff', 
+//                 '&:hover': {
+//                   bgcolor: '#59a3f7', 
+//                 },
+//               }}
+//             >
+//               Enviar
+//             </Button>
+//           </Grid>
+//         </Grid>
+//       </CardContent>
+//     </Card>
+//   );
+// }
+
+// export default MandarMensajes;
+
+
 import { Avatar, Button, Card, CardContent, CardHeader, Grid, TextField } from '@mui/material';
 import { useState } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../../firebase/Conexion.js";
 import { getAuth } from "firebase/auth";
 
@@ -18,13 +221,15 @@ const MandarMensajes = ({ onMessageSent }) => {
     const mensaje = {
       texto: texto,
       correo: correo,
-      photoURL: avatar // Asegúrate de agregar la URL de la foto al mensaje
+      photoURL: avatar,
+      displayName: name,
+      timestamp: serverTimestamp() // Agregar el timestamp al mensaje
     };
-
+    
     try {
       const mensajes = collection(db, "mensajes");
       const docRef = await addDoc(mensajes, mensaje);
-      mensaje.id = docRef.id; // Agregar el ID del documento generado a la propiedad mensaje
+      mensaje.id = docRef.id; // Agregar el ID del documento generado al mensaje
       onMessageSent(mensaje); // Llamar a la función onMessageSent con el nuevo mensaje
       setTexto(""); // Limpiar el campo de texto después de enviar el mensaje
     } catch (error) {
@@ -45,7 +250,7 @@ const MandarMensajes = ({ onMessageSent }) => {
       />
       <CardContent>
         <Grid container spacing={2} alignItems="center">
-          <Grid item xs={12} sm={12}>
+          <Grid item xs={12} sm={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
             <TextField
               fullWidth
               variant="outlined"
@@ -94,6 +299,6 @@ const MandarMensajes = ({ onMessageSent }) => {
       </CardContent>
     </Card>
   );
-};
+}
 
 export default MandarMensajes;
--- proyecto-api/src/components/cards/CardTraerMensajes.jsx
+++ proyecto-api/src/components/cards/CardTraerMensajes.jsx
@@ -1,4 +1,74 @@
 
+// import Avatar from '@mui/material/Avatar';
+// import { Card, CardContent, Box } from '@mui/material';
+// import * as React from 'react';
+
+// const TraerMensajes = ({ mensajes }) => {
+//   return (
+//     <div>
+//       {mensajes.map((mensaje) => (
+//         <Card 
+//           key={mensaje.id} 
+//           sx={{ 
+//             marginTop: '10px', 
+//             width: '100%', 
+//             borderRadius: '8px',
+//             boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+//           }}
+//         >
+//           <CardContent>
+//             <Box display="flex" alignItems="center" mb={2}>
+//               <Avatar alt={mensaje.correo} src={mensaje.photoURL || ""} sx={{ width: 40, height: 40, marginRight: '10px' }} />
+//               <Box>
+//                 <p style={{ marginBottom: '4px', color: 'gray' }}>{mensaje.correo}</p>
+//                 <p>{mensaje.texto}</p>
+//               </Box>
+//             </Box>
+//           </CardContent>
+//         </Card>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default TraerMensajes;
+
+
+// import Avatar from '@mui/material/Avatar';
+// import { Card, CardContent, Box } from '@mui/material';
+// import * as React from 'react';
+
+// const TraerMensajes = ({ mensajes }) => {
+//   return (
+//     <div>
+//       {mensajes.slice().reverse().map((mensaje) => (
+//         <Card 
+//           key={mensaje.id} 
+//           sx={{ 
+//             marginTop: '10px', 
+//             width: '100%', 
+//             borderRadius: '8px',
+//             boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+//           }}
+//         >
+//           <CardContent>
+//             <Box display="flex" alignItems="center" mb={2}>
+//               <Avatar alt={mensaje.correo} src={mensaje.photoURL || ""} sx={{ width: 40, height: 40, marginRight: '10px' }} />
+//               <Box>
+//                 <p style={{ marginBottom: '4px', color: 'gray' }}>{mensaje.correo}</p>
+//                 <p>{mensaje.texto}</p>
+//               </Box>
+//             </Box>
+//           </CardContent>
+//         </Card>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default TraerMensajes;
+
+
 import Avatar from '@mui/material/Avatar';
 import { Card, CardContent, Box } from '@mui/material';
 import * as React from 'react';
