a327731510ad2966e29e3d485d4127b5f3e4f520
Andrea <actassi@gmail.com>
2024-05-15 11:37:23 -0300

    formateando archivos home y profile

--- proyecto-api/src/pages/home/HomePage.jsx
+++ proyecto-api/src/pages/home/HomePage.jsx
@@ -1,107 +1,24 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { CssBaseline, Box, Grid, Container, TextField, InputAdornment } from '@mui/material';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import {
+  CssBaseline,
+  Box,
+  Grid,
+  Container,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
 import { collection, getDocs, query, orderBy } from "firebase/firestore";
-import { db } from '../../firebase/Conexion.js';
+import { db } from "../../firebase/Conexion.js";
+import DrawerLeft from "../../components/drawer/Drawer";
+import { Search as SearchIcon } from "@mui/icons-material";
+import MandarMensajes from "../../components/cards/CardMandarMensajes"; // Ajusta la ruta según sea necesario
+import GiphyViewer from "../../components/apis/giphy/Giphy"; // Importar GiphyViewer
+import TraerMensajes from "../../components/cards/CardTraerMensajes";
+import UnderlineTabs from "../../components/tabs/Tabs1.jsx";
+import { MdOutlineSettings } from "react-icons/md";
 
-// import DrawerLeft from '../../components/drawer/Drawer';
-// import { Search as SearchIcon } from '@mui/icons-material';
-// import LogOut from '../login/LogOut';
-// import MandarMensajes from '../../components/cards/CardMandarMensajes'; // Ajusta la ruta según sea necesario
-// import GiphyViewer from '../../components/apis/giphy/Giphy'; // Importar GiphyViewer
-// import TraerMensajes from '../../components/cards/CardTraerMensajes';
-
-// const margenSup = '80px';
-
-// export default function HomePage() {
-//   const [mensajes, setMensajes] = useState([]);
-
-//   useEffect(() => {
-//     // Función para obtener los mensajes desde Firebase
-//     const obtenerMensajes = async () => {
-//       const mensajesSnapshot = await getDocs(collection(db, "mensajes"));
-//       const mensajesList = mensajesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-//       setMensajes(mensajesList.reverse()); // Ordenar los mensajes de último a primero
-//     };
-
-//     // Llamar a la función para obtener los mensajes cuando el componente se monta
-//     obtenerMensajes();
-//   }, []);
-
-//   const handleNuevoMensaje = (mensaje) => {
-//     setMensajes(prevMensajes => [mensaje, ...prevMensajes]);
-//   };
-
-//   return (
-//     <React.Fragment>
-//       <CssBaseline />
-//       <Container maxWidth="lg" sx={{ marginLeft: '-10px', marginTop: '-80px' }}>
-//         <Grid container spacing={2}>
-//           {/* DrawerLeft */}
-//           <Grid item xs={12} md={3} sx={{ marginTop: margenSup, position: 'sticky', top: '0', alignSelf: 'flex-start' }}>
-//             <DrawerLeft selectedPath={'/home'} />
-//           </Grid>
-//           {/* Contenido centrado */}
-//           <Grid item xs={12} md={6} sx={{ marginTop: margenSup }}>
-//             <Box
-//               display="flex"
-//               alignItems="center"
-//               justifyContent="space-between"
-//               border="1px solid rgba(0, 0, 0, 0.1)" // Borde gris claro
-//               backgroundColor="transparent"
-//               borderRadius="1px"
-//               padding="0.1rem"
-//             >
-//               {/* <UnderlineTabs />
-//               <MdOutlineSettings size={20} color="black" /> */}
-//             </Box>
-//             <MandarMensajes onMessageSent={handleNuevoMensaje} />
-//             {/* Mostrar los mensajes (ahora ordenados) */}
-//             <TraerMensajes mensajes={mensajes} />
-//             {/* Mostrar GiphyViewer */}
-//             <GiphyViewer />
-//           </Grid>
-//           <Grid item xs={12} md={3} sx={{ marginTop: margenSup }}>
-//             <LogOut />
-//             <TextField 
-//               fullWidth
-//               variant="outlined"
-//               placeholder="Search..."
-//               InputProps={{
-//                 startAdornment: (
-//                   <InputAdornment position="start">
-//                     <SearchIcon />
-//                   </InputAdornment>
-//                 ),
-//                 sx: {
-//                   bgcolor: '#ffffff', // Fondo gris claro
-//                   borderRadius: '999px', // Borde redondeado
-//                   '& input': {
-//                     borderRadius: '999px', // Borde redondeado
-//                   },
-//                 },
-//               }}
-//             />
-//           </Grid>
-//         </Grid>
-//       </Container>
-//     </React.Fragment>
-//   );
-// }
-
-
-
-
-import DrawerLeft from '../../components/drawer/Drawer';
-import { Search as SearchIcon } from '@mui/icons-material';
-import MandarMensajes from '../../components/cards/CardMandarMensajes'; // Ajusta la ruta según sea necesario
-import GiphyViewer from '../../components/apis/giphy/Giphy'; // Importar GiphyViewer
-import TraerMensajes from '../../components/cards/CardTraerMensajes';
-import UnderlineTabs from '../../components/tabs/Tabs1.jsx';
-import { MdOutlineSettings } from 'react-icons/md';
-import LogOut from '../login/LogOut'
-
-const margenSup = '80px';
+const margenSup = "80px";
 
 export default function HomePage() {
   const [mensajes, setMensajes] = useState([]);
@@ -109,9 +26,15 @@ export default function HomePage() {
   useEffect(() => {
     // Función para obtener los mensajes desde Firebase
     const obtenerMensajes = async () => {
-      const mensajesQuery = query(collection(db, "mensajes"), orderBy("timestamp", "desc"));
+      const mensajesQuery = query(
+        collection(db, "mensajes"),
+        orderBy("timestamp", "desc")
+      );
       const mensajesSnapshot = await getDocs(mensajesQuery);
-      const mensajesList = mensajesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      const mensajesList = mensajesSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
       setMensajes(mensajesList); // Ya están ordenados por timestamp descendente
     };
 
@@ -120,17 +43,30 @@ export default function HomePage() {
   }, []);
 
   const handleNuevoMensaje = (mensaje) => {
-    setMensajes(prevMensajes => [mensaje, ...prevMensajes]);
+    setMensajes((prevMensajes) => [mensaje, ...prevMensajes]);
   };
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="lg" sx={{ marginLeft: '-10px', marginTop: '-80px' }}>
+      <Container maxWidth="lg" sx={{ marginLeft: "-10px", marginTop: "-80px" }}>
         <Grid container spacing={2}>
           {/* DrawerLeft */}
-          <Grid item xs={12} md={3} sx={{ marginTop: margenSup, position: 'sticky', top: '0', alignSelf: 'flex-start' }}>
-            <DrawerLeft selectedPath={'/home'}  sx={{display: 'flex', height: '100%'}} />
+          <Grid
+            item
+            xs={12}
+            md={3}
+            sx={{
+              marginTop: margenSup,
+              position: "sticky",
+              top: "0",
+              alignSelf: "flex-start",
+            }}
+          >
+            <DrawerLeft
+              selectedPath={"/home"}
+              sx={{ display: "flex", height: "100%" }}
+            />
           </Grid>
           {/* Contenido centrado */}
           <Grid item xs={12} md={6} sx={{ marginTop: margenSup }}>
@@ -144,7 +80,7 @@ export default function HomePage() {
               padding="0.1rem"
             >
               <UnderlineTabs />
-              <MdOutlineSettings size={20} color="black" /> 
+              <MdOutlineSettings size={20} color="black" />
             </Box>
             <MandarMensajes onMessageSent={handleNuevoMensaje} />
             {/* Mostrar los mensajes (ahora ordenados) */}
@@ -153,24 +89,21 @@ export default function HomePage() {
             <GiphyViewer />
           </Grid>
           <Grid item xs={12} md={3} sx={{ marginTop: margenSup }}>
-          
-            <TextField 
+            <TextField
               fullWidth
               variant="outlined"
               placeholder="Search..."
               InputProps={{
                 startAdornment: (
-
                   <InputAdornment position="start">
-
                     <SearchIcon />
                   </InputAdornment>
                 ),
                 sx: {
-                  bgcolor: '#ffffff', // Fondo gris claro
-                  borderRadius: '999px', // Borde redondeado
-                  '& input': {
-                    borderRadius: '999px', // Borde redondeado
+                  bgcolor: "#ffffff", // Fondo gris claro
+                  borderRadius: "999px", // Borde redondeado
+                  "& input": {
+                    borderRadius: "999px", // Borde redondeado
                   },
                 },
               }}
--- proyecto-api/src/pages/profile/ProfilePage.jsx
+++ proyecto-api/src/pages/profile/ProfilePage.jsx
@@ -1,21 +1,28 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { CssBaseline, Box, Grid, Container, TextField, InputAdornment } from '@mui/material';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import {
+  CssBaseline,
+  Box,
+  Grid,
+  Container,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
 import { collection, getDocs, query, where, orderBy } from "firebase/firestore";
-import { db } from '../../firebase/Conexion.js';
+import { db } from "../../firebase/Conexion.js";
 import BlueButton from "../../components/buttons/BlueButton.jsx";
 import EditProfileModal from "./EditProfileModal.jsx";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 
-import DrawerLeft from '../../components/drawer/Drawer.jsx';
-import { Search as SearchIcon } from '@mui/icons-material';
-import GiphyViewer from '../../components/apis/giphy/Giphy.jsx';
-import TraerMensajes from '../../components/cards/CardTraerMensajes.jsx';
-import UnderlineTabs from '../../components/tabs/Tabs1.jsx';
-import { MdOutlineSettings } from 'react-icons/md';
-import LogOut from '../login/LogOut.jsx';
+import DrawerLeft from "../../components/drawer/Drawer.jsx";
+import { Search as SearchIcon } from "@mui/icons-material";
+import GiphyViewer from "../../components/apis/giphy/Giphy.jsx";
+import TraerMensajes from "../../components/cards/CardTraerMensajes.jsx";
+import UnderlineTabs from "../../components/tabs/Tabs1.jsx";
+import { MdOutlineSettings } from "react-icons/md";
+import LogOut from "../login/LogOut.jsx";
 
-const margenSup = '80px';
+const margenSup = "80px";
 
 export default function ProfilePage() {
   const [mensajes, setMensajes] = useState([]);
@@ -33,7 +40,10 @@ export default function ProfilePage() {
           orderBy("timestamp", "desc")
         );
         const mensajesSnapshot = await getDocs(mensajesQuery);
-        const mensajesList = mensajesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        const mensajesList = mensajesSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
         setMensajes(mensajesList);
       }
     };
@@ -51,11 +61,24 @@ export default function ProfilePage() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="lg" sx={{ marginLeft: '-10px', marginTop: '-80px' }}>
+      <Container maxWidth="lg" sx={{ marginLeft: "-10px", marginTop: "-80px" }}>
         <Grid container spacing={2}>
           {/* DrawerLeft */}
-          <Grid item xs={12} md={3} sx={{ marginTop: margenSup, position: 'sticky', top: '0', alignSelf: 'flex-start' }}>
-            <DrawerLeft selectedPath={'/home'}  sx={{display: 'flex', height: '100%'}} />
+          <Grid
+            item
+            xs={12}
+            md={3}
+            sx={{
+              marginTop: margenSup,
+              position: "sticky",
+              top: "0",
+              alignSelf: "flex-start",
+            }}
+          >
+            <DrawerLeft
+              selectedPath={"/home"}
+              sx={{ display: "flex", height: "100%" }}
+            />
           </Grid>
           {/* Contenido centrado */}
           <Grid item xs={12} md={6} sx={{ marginTop: margenSup }}>
@@ -69,17 +92,15 @@ export default function ProfilePage() {
               padding="0.1rem"
             >
               <UnderlineTabs />
-              <MdOutlineSettings size={20} color="black" /> 
+              <MdOutlineSettings size={20} color="black" />
             </Box>
             <BlueButton onClick={handleOpen}>Editar Perfil</BlueButton>
             <EditProfileModal open={open} handleClose={handleClose} />
             {/* Mostrar los mensajes (ahora ordenados) */}
             <TraerMensajes mensajes={mensajes} />
-           
           </Grid>
           <Grid item xs={12} md={3} sx={{ marginTop: margenSup }}>
-            
-            <TextField 
+            <TextField
               fullWidth
               variant="outlined"
               placeholder="Search..."
@@ -90,10 +111,10 @@ export default function ProfilePage() {
                   </InputAdornment>
                 ),
                 sx: {
-                  bgcolor: '#ffffff', // Fondo gris claro
-                  borderRadius: '999px', // Borde redondeado
-                  '& input': {
-                    borderRadius: '999px', // Borde redondeado
+                  bgcolor: "#ffffff", // Fondo gris claro
+                  borderRadius: "999px", // Borde redondeado
+                  "& input": {
+                    borderRadius: "999px", // Borde redondeado
                   },
                 },
               }}
